fe71fc818852a632805e3f2ebe7727e8379ae47a
Chehak Sharma <ch3hak@gmail.com>
2026-01-10 01:14:57 +0530

    fix: Auth

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const {connectDb} = require('./utils/database'); // Assuming utils.js/database exports connectDb
+const {connectDb} = require('./utils/database');
 const cors = require("cors");
 const cookieParser = require('cookie-parser');
 require('dotenv').config(); 
